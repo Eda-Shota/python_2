00cba1f8bf36c9a76d859b46f89ede0f976e60ee
Eda-Shota <edash0086@gmail.com>
2023-05-25 13:13:49 +0900

    [Add]

--- base_study/16_DataBase/02.py
+++ base_study/16_DataBase/02.py
@@ -2,4 +2,30 @@
 # http.serverモジュール HTPPサーバーの機能を提供する
 # コマンドラインへ　python -m http.server --cgi
 
-# CGIプログラムを書いてみる
+# Ｗebフレームワークを使用する
+# Bottleフレームワーク
+pip install bottle
+
+import bottle
+
+html = '''
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>Bottle</title>
+    </head>
+    <body>
+        {}
+    </body>
+</html>    
+'''
+@bottle.route("/hello") # /helloにアクセスするとHello!
+def helo():
+    return html.format("Hello!")
+
+@bottle.route("/bye")
+def bye():
+    return html.format("Bye!") # /byeにアクセスするとBye!
+bottle.run(host="localhost", port=8000)
+
